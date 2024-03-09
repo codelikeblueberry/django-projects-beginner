f2159b7e289cd59339a36a379abe0ae40844109e
Swatantra <102201952+codelikeblueberry@users.noreply.github.com>
2024-03-09 12:46:08 +0530

    moving from master to main

--- projectImageUploader/ImageUploaderPro/homeapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from.models import ImageData
-# Register your models here.
-@admin.register(ImageData)
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ['id','image_name','image_date_time']
-    
--- projectImageUploader/ImageUploaderPro/homeapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class ImageData(models.Model):
-    image_name = models.ImageField(upload_to="images")
-    image_date_time = models.DateTimeField(auto_now_add=True)
